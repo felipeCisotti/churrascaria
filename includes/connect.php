0b36a4df8e976fd86267aeed9f5c6b3a39edcee3
felipeCisotti <felipe.cisotti@aluno.senai.br>
2025-11-23 20:05:22 -0300

    Remove some comments

--- includes/connect.php
+++ includes/connect.php
@@ -1,12 +1,12 @@
 <?php
-// Conexão PDO e MySQLi compatível (ajuste host, db, user, pass)
+
 $DB_HOST = 'localhost';
 $DB_NAME = 'churrascaria';
 $DB_USER = 'root';
 $DB_PASS = '';
 
 try {
-    // PDO (usado em cardapio_dash.php)
+
     $dsn = "mysql:host={$DB_HOST};dbname={$DB_NAME};charset=utf8mb4";
     $pdo = new PDO($dsn, $DB_USER, $DB_PASS, [
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
@@ -14,7 +14,7 @@ try {
         PDO::ATTR_EMULATE_PREPARES => false,
     ]);
 
-    // MySQLi (usado em login.php / register.php)
+
     $connect = new mysqli($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME);
     if ($connect->connect_errno) {
         throw new Exception("MySQLi connect error: " . $connect->connect_error);
